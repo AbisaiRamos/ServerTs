d366f76ca553c5b5a85e74159f0f0456f881abc7
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-11-21 22:35:18 -0600

    getOneByEmail interface implemented

--- src/lib/User/domain/UserRepository.ts
+++ src/lib/User/domain/UserRepository.ts
@@ -6,6 +6,7 @@ export interface UserRepository {
     create(user: User): Promise<void>
     getAll(): Promise<User[]>
     getOneById(id: UserId): Promise<User | null>
+    getOneByEmail(email: string): Promise<User | null>
     edit(user: User): Promise<void>
     delete(id: UserId): Promise<void>
 }
