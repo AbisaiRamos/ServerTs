8735739a7e239fd3f40e5b337de1bd22ebae19ea
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-11-24 00:21:06 -0600

    update repository  for implement userEmail and password

--- src/lib/User/infraestructure/InMemoryRepository.ts
+++ src/lib/User/infraestructure/InMemoryRepository.ts
@@ -1,9 +1,15 @@
 import { UserRepository } from "../domain/UserRepository";
 import { User } from "../domain/User";
 import { UserId } from "../domain/UserId";
+import { UserName } from "../domain/UserName";
+import { UserEmail } from "../domain/UserEmail";
+import { UserPassword } from "../domain/UserPassword";
+import { UserCreatedAt } from "../domain/UserCreateAt";
 
 export class InMemoryRepository implements UserRepository {
-    private users: User[] = []
+    private users: User[] = [
+        new User(new UserId('12345'), new UserName('Abisai'), new UserEmail('abisai@gmail.com'), new UserPassword('administrator'), new UserCreatedAt(new Date()))
+    ]
 
     async create(user: User): Promise<void> {
         this.users.push(user)
